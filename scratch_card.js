288f471c57efe727f6dccd93a32bdb7e6a663dd9
Tobi Akanji <tobia807@gmail.com>
2019-09-17 17:58:09 +0100

    Modified db.json, index.html, mystyle.css and scratch_card.js

--- scratch_card.js
+++ scratch_card.js
@@ -1,63 +1,66 @@
 $(document).ready(()=>{
-    const header=$("#page-name");
-    const loginPageLabel="Login Page";
-    const signUpPageLabel="Signup Page";
-    const purchasePageLabel="Card Purchase";
-    const invoiceWindow = "Invoice";
-    const generatedPINPage = "Scratch Card PINS";
-    const signUpLink=$("#sign-up-link");
-    const signInLink=$("#sign-in-link");
-    const signUpFrame=$("#signup-frame");
-    const signInFrame=$("#user-login-frame");
-    const purchaseFrame=$("#purchase-frame");
-    const invoiceModal=$("#invoice-modal");
-    const purchaseSummary=$(".purchase-summary");
-    const buyHoldSection=$(".buy-hold-section");
-    const buyButton=$(".buy-button");
-    const holdButton=$(".hold-button");
-    const cartButton=$(".cart-button");
-    const priceTotalFigure=$(".price-total-figure");
-
-    
-    header.parent().css({
-      "height": "50px",
-      "background": "grey",
-      "margin-bottom":"10px"
+  const header=$("#page-name");
+  const loginPageLabel="Login Page";
+  const signUpPageLabel="Signup Page";
+  const purchasePageLabel="Card Purchase";
+  const invoiceWindow = "Invoice";
+  const generatedPINPage = "Scratch Card PINS";
+  const signUpLink=$("#sign-up-link");
+  const signInLink=$("#sign-in-link");
+  const signUpFrame=$("#signup-frame");
+  const signInFrame=$("#user-login-frame");
+  const purchaseFrame=$("#purchase-frame");
+  const invoiceModal=$("#invoice-modal");
+  const purchaseSummary=$(".purchase-summary");
+  const buyHoldSection=$(".buy-hold-section");
+  const buyButton=$(".buy-button");
+  const holdButton=$(".hold-button");
+  const cartButton=$(".cart-button");
+  const priceTotalFigure=$(".price-total-figure");
+  const total200cell=$("#mult-price-200");
+  const total500cell=$("#mult-price-500");
+  const total1000cell=$("#mult-price-1000");
+  const total1000cell=$(".pin-display-page");
+  
+  header.parent().css({
+    "height": "50px",
+    "background": "grey",
+    "margin-bottom":"10px"
+  });
+  header.text(loginPageLabel);
+  header.css({
+    "color":"#EEEEEE",
+    "text-align": "center",
+    "vertical-align":"middle",
+    "margin":"auto",
+    "transform":"translate(0%,50%)"
+  });
+  signInLink.on("click", (e)=>{
+    signUpFrame.fadeOut(1000,"swing", ()=>{
+      signInFrame.fadeIn(1000);
+      header.text(loginPageLabel);
     });
-    header.text(loginPageLabel);
-    header.css({
-      "color":"#EEEEEE",
-      "text-align": "center",
-      "vertical-align":"middle",
-      "margin":"auto",
-      "transform":"translate(0%,50%)"
+  });
+  signUpLink.on("click", (e)=>{
+    signInFrame.fadeOut(1000, "swing", ()=>{
+      signUpFrame.fadeIn(1000);
+      header.text(signUpPageLabel);
     });
-    signInLink.on("click", (e)=>{
-      signUpFrame.fadeOut(1000,"swing", ()=>{
-        signInFrame.fadeIn(1000);
-        header.text(loginPageLabel);
-      });
-    });
-    signUpLink.on("click", (e)=>{
-      signInFrame.fadeOut(1000, "swing", ()=>{
-        signUpFrame.fadeIn(1000);
-        header.text(signUpPageLabel);
-      });
-    });
+  });
 
-    cartButton.on("click",  ()=>{
+  cartButton.on("click",  ()=>{
 
-    });
+  });
 
-    var calc=""+((purchaseFrame.height()/2)-3*buyButton.height())+"px";
-    invoiceModal.css({"height":purchaseSummary.css("height")});
-    buyHoldSection.css("top", calc);
+  var calc=""+((purchaseFrame.height()/2)-3*buyButton.height())+"px";
+  invoiceModal.css({"height":purchaseSummary.css("height")});
+  buyHoldSection.css("top", calc);
 
-    buyButton.on("click",  ()=>{
+  buyButton.on("click",  ()=>{
 
-    });
-    holdButton.on("click",  ()=>{
+  });
+  holdButton.on("click",  ()=>{
 
-    });
-
+  });
+  
   });
