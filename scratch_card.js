edd349fbaaee1b28ba8c1d164359215519816f18
Tobi Akanji <tobia807@gmail.com>
2019-09-19 12:12:07 +0100

    Updated my database to receive and send data for user login and scratchcard storage via jquery in my scratch_card.js file, and restructured that database db.json. Updated the user_page.html

--- scratch_card.js
+++ scratch_card.js
@@ -21,6 +21,7 @@ $(document).ready(()=>{
   //variables for purchase page
   const purchasePageLabel="Card Purchase";
   const purchaseFrame=$("#purchase-frame");
+  const cartBtn=$(".cart-button");
   const quantity200Input=$("#quantity-200 input");
   const quantity500Input=$("#quantity-500 input");
   const quantity1000Input=$("#quantity-1000 input");
@@ -44,7 +45,10 @@ $(document).ready(()=>{
   });
   //styling function for invoice
   /*var calc=""+((purchaseFrame.height()/2)-3*buyButton.height())+"px";
-  invoiceModal.css({"height":purchaseSummary.css("height")});*/
+  invoiceModal.css({"height":purchaseSummary.css("height")});
+  
+  buyHoldSection.css("top", calc);
+  */
   
   
   //functions for switching the sign-in sign-up links
@@ -74,14 +78,20 @@ $(document).ready(()=>{
       $.ajax({
         url:"http://localhost:3000/users",
         method:"GET",
-        /*success:(data,staus)=>{
-          alert(JSON.stringify(staus));
-        }*/
         data: {"username":usernameSignin.val()},
         dataType: "json",
-        success:(data,staus)=>{
-          if(data!==true){alert("not true");} else{alert("true")}
-          
+        success:(data,status)=>{
+          let stringUsername=JSON.stringify(data);
+          alert(data.length);
+          if(data.length!==0){
+            data.forEach((e)=>{
+            if (e.password===passwordSignin.val()) {
+              alert("correct username and password")
+            } else{
+              alert("wrong PASSWORD")}})
+          } else{
+            alert("wrong USERNAME")
+          }
         },
         error:()=>{
           alert("error")
@@ -91,38 +101,205 @@ $(document).ready(()=>{
   });
   
   
+  //function for sign up
+  let signUpName=signUpUsername.val();
+  let signUpEmail=emailSignUp.val();
+  let signUpPass=passwordSignup.val();
+  let signUpConfirm=confirmPassword.val();
+  signUpBtn.on("click", ()=>{
+    if(signUpEmail.length===0){
+      alert("input email");
+      emailSignUp.attr("placeholder","e.g: decadev@decagon.com");
+    } else if(signUpPass.length===0){
+      alert("input password");
+    } else if(signUpConfirm.length===0){
+      alert("confirm password");
+    } else {
+      if(signUpPass===signUpConfirm){
+        let user={
+          "username": signUpUsername,
+          "email": signUpEmail,
+          "phone_number": "+234",
+          "rank": "Client",
+          "password": signUpConfirm
+        };
+        $.ajax({
+          url:"http://localhost:3000/users",
+          method:"POST",
+          data:JSON.stringify(user),
+          dataType:"json",
+          success:(data,status)=>{
+            alert("sucessful sign up")
+          }
+          
+        });
+      }
+    }
+  });
+  
   //functions for purchase
-  var allPrices=[];
+  let allPrices=[];
+  let pins200=[];
+  let pins500=[];
+  let pins1000=[];
+  let per200="";
+  let per500="";
+  let per1000="";
+  
+  //generate pins for 200
+  let cart1;
   quantity200Input.on("keyup",(e)=>{
-    if(quantity200Input.val().length!==0){
-      let amount200=parseInt(quantity200Input.val())*200;
+    if(quantity200Input.val().length!==0 && quantity200Input.val()!=="0"){
+      let quantity200=parseInt(quantity200Input.val());
+      let amount200=quantity200*200;
       total200Cell.text(amount200);
       allPrices[0]=amount200;
+      for(let i=0;i<quantity200;i++){
+        for(let j=0;j<17;j++){
+          if(j===0){
+            per200+=2;
+          } else if(j===5){
+            per200+=2;
+          } else if(j===12){
+            per200+=2;
+          } else{
+            per200+=Math.floor(Math.random()*10);
+          }
+        }
+        if(!pins200.includes(per200)){
+           pins200.push(per200);
+        }
+        per200="";
+      }
+      cart1={"id": 1, "price": "200", "cards": pins200};
     } else{total200Cell.text('')}
   });
   
+  //generate pins for 500
+  let cart2;
   quantity500Input.on("keyup",(e)=>{
-    if(quantity500Input.val().length!==0){
-      let amount500=parseInt(quantity500Input.val())*500;
+    if(quantity500Input.val().length!==0 && quantity500Input.val()!=="0"){
+      let quantity500=parseInt(quantity500Input.val());
+      let amount500=quantity500*500;
       total500Cell.text(amount500);
       allPrices[1]=amount500;
-      
+      for(let i=0;i<quantity500;i++){
+        for(let j=0;j<17;j++){
+          if(j===0){
+            per500+=5;
+          } else if(j===5){
+            per500+=5;
+          } else if(j===12){
+            per500+=5;
+          } else{
+             per500+=Math.floor(Math.random()*10);
+          }
+        }   
+        if(!pins500.includes(per500)){
+           pins500.push(per500);
+        }
+        per500="";
+      }
+      cart2={"id": 2, "price": "500", "cards": pins500};
     } else{total500Cell.text('')}
   });
   
+  //generate pins for 1000
+  let cart3;
   quantity1000Input.on("keyup",(e)=>{
-    if(quantity1000Input.val().length!==0){
-      let amount1000=parseInt(quantity1000Input.val())*1000;
+    if(quantity1000Input.val().length!==0 && quantity1000Input.val()!=="0"){
+      let quantity1000=parseInt(quantity1000Input.val());
+      let amount1000=quantity1000*1000;
       total1000Cell.text(amount1000);
       allPrices[2]=amount1000;
-    } else{total1000Cell.text('')}});
-  
-  cartButton.on("click",  ()=>{
-    
+      for(let i=0;i<quantity1000;i++){
+        for(let j=0;j<17;j++){
+          if(j===0){
+            per1000+=3;
+          } else if(j===5){
+            per1000+=3;
+          } else if(j===12){
+            per1000+=3;
+          } else{
+            per1000+=Math.floor(Math.random()*10);
+          }
+        }
+        if(!pins1000.includes(per1000)){
+           pins1000.push(per1000);
+        }
+        per1000="";
+      }
+      cart3={"id": 3, "price": "1000", "cards": pins1000};
+    } else{total1000Cell.text('')}
   });
-
   
-  buyHoldSection.css("top", calc);
+  //contents to be added to cart in the database
+  /*let carts=[
+    {"id": 1, "price": "200", "cards": pins200},
+    {"id": 2, "price": "500", "cards": pins500},
+    {"id": 3, "price": "1000", "cards": pins1000}
+  ];*/
+  
+  let scratchcardCart;
+  let allscratchcards=[];
+  cartBtn.on("click",  ()=>{
+    if(quantity200Input.val().length!==0 && quantity200Input.val()!=="0" && quantity500Input.val().length!==0 && quantity500Input.val()!=="0" && quantity1000Input.val().length!==0 && quantity1000Input.val()!=="0"){
+      //scratchcardCart=JSON.stringify(carts);
+      for(let i=0;i<pins200.length;i++){
+        $.ajax({
+          url:"http://localhost:3000/card_history",
+          method:"POST",
+          data:{
+            "id":i+1,
+            "200":pins200[i]
+          },
+          dataType:"json",
+          success:(data,status)=>{
+            if(i===pins200.length-1){
+              allscratchcards.push(pins200);
+              alert("done with N200");
+            }
+          },
+          error:()=>{}
+        });
+      }
+      for(let i=0;i<pins500.length;i++){
+        $.ajax({
+          url:"http://localhost:3000/card_history",
+          method:"POST",
+          data:{
+            "id":pins200.length+i+1,
+            "500":pins500[i]},
+          dataType:"json",
+          success:(data,status)=>{
+            if(i===pins500.length-1){
+              allscratchcards.push(pins500);
+              alert("done with N500");
+            }
+          },
+          error:()=>{}
+        });
+      }
+      for(let i=0;i<pins1000.length;i++){
+        $.ajax({
+          url:"http://localhost:3000/card_history",
+          method:"POST",
+          data:{
+            "id":pins500.length+i+1,
+            "1000":pins1000[i]},
+          dataType:"json",
+          success:(data,status)=>{
+            if(i===pins1000.length-1){
+              allscratchcards.push(pins1000);
+              alert("done with N1000");
+            }
+          },
+          error:()=>{}
+        });
+      }
+    }
+  });
+  
 
   buyButton.on("click",  ()=>{
 
